cdafe9e76c320896ab0ab16130e35c1e4ac4ed71
Thị Thùy Dung Lê <Th Thy Dung L>
2017-06-17 08:22:29 +0700

    Set default colors

--- App/Themes/Colors.js
+++ App/Themes/Colors.js
@@ -1,27 +1,42 @@
 const colors = {
-  background: '#1F0808',
-  clear: 'rgba(0,0,0,0)',
-  facebook: '#3b5998',
-  transparent: 'rgba(0,0,0,0)',
-  silver: '#F7F7F7',
-  steel: '#CCCCCC',
-  error: 'rgba(200, 0, 0, 0.8)',
-  ricePaper: 'rgba(255,255,255, 0.75)',
-  frost: '#D8D8D8',
-  cloud: 'rgba(200,200,200, 0.35)',
-  windowTint: 'rgba(0, 0, 0, 0.4)',
-  panther: '#161616',
-  charcoal: '#595959',
-  coal: '#2d2d2d',
-  bloodOrange: '#fb5f26',
-  snow: 'white',
-  ember: 'rgba(164, 0, 48, 0.5)',
-  fire: '#e73536',
-  drawer: 'rgba(30, 30, 29, 0.95)',
-  eggplant: '#251a34',
-  border: '#483F53',
-  banner: '#5F3E63',
-  text: '#E0D7E5'
+  turquoise: '#1abc9c',
+  greenSea: '#16a085',
+  emerald: '#2ecc71',
+  nephritis: '#27ae60',
+  peterRiver: '#3498db',
+  belizeHole: '#2980b9',
+  amethyst: '#9b59b6',
+  wisteria: '#8e44ad',
+  wetAsphalt: '#34495e',
+  midnightBlue: '#2c3e50',
+  sunFlower: '#f1c40f',
+  orange: '#f39c12',
+  carrot: '#e67e22',
+  pumpkin: '#d35400',
+  alizarin: '#e74c3c',
+  pomegranate: '#c0392b',
+  cloudes: '#ecf0f1',
+  silver: '#bdc3c7',
+  concrete: '#95a5a6',
+  asbestos: '#7f8c8d'
 };
 
-export default colors;
+const defaultColor = {
+  primary: colors.alizarin,
+  primaryDark: colors.pomegranate,
+  secondary: colors.cloudes,
+  secondaryDark: colors.silver,
+  success: colors.emerald,
+  successDark: colors.nephritis,
+  warning: colors.sunFlower,
+  warningDark: colors.orange,
+  info: colors.peterRiver,
+  infoDark: colors.belizeHole,
+  danger: colors.alizarin,
+  dangerDark: colors.pomegranate
+};
+
+export default {
+  ...colors,
+  ...defaultColor
+};
