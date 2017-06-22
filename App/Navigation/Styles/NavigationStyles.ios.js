1e68219738c79f1710e8194efc9a60807d3eebf7
Tri Pham <pvtri96@gmail.com>
2017-06-22 11:34:12 +0700

    Update App navigation header style

---
+++ App/Navigation/Styles/NavigationStyles.ios.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from '../../Themes/';
+//import { Constants } from 'expo';
+
+export default StyleSheet.create({
+  header: {
+    //backgroundColor: Colors.primary,
+    marginTop: 0
+  },
+  title: {
+    color: Colors.secondary
+  },
+  back: {
+    color: Colors.secondary,
+    backgroundColor: Colors.secondary
+  }
+});
