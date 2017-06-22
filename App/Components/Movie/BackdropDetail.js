2c003713d7af5a51b5ed61d6a4d8d8735bb9ca6c
Tri Pham <pvtri96@gmail.com>
2017-06-22 16:06:50 +0700

    show more information in BackDropDetail

--- App/Components/Movie/BackdropDetail.js
+++ App/Components/Movie/BackdropDetail.js
@@ -9,18 +9,41 @@ import Rating from 'react-native-star-rating';
 import FitImage from 'react-native-fit-image';
 import styles from './Styles/BackdropDetailStyles';
 import colors  from '../../Themes/Colors';
+import fonts  from '../../Themes/Fonts';
 import PropTypes from 'prop-types';
 import { THEMOVIEDB_IMAGE_SRC } from 'react-native-dotenv';
 
 class BackdropMovieDetail extends Component {
 
-  render() {
-    const movie = this.props.movie;
-    const genres =  movie.genres ? movie.genres.map((genre, index) => {
+  _renderGenres (movie) {
+    return movie.genres.map((genre, index) => {
       if (index == (movie.genres.length - 1))
         return genre.name;
       return `${genre.name}, `;
-    }) : null;
+    });
+  }
+
+  _renderProductionCompanies (movie) {
+    return movie.production_companies.map((company, index) => {
+      if (index == (movie.production_companies.length - 1))
+        return company.name;
+      return `${company.name}, `;
+    });
+  }
+
+  _renderProductionCountries (movie) {
+    return movie.production_countries.map((country, index) => {
+      if (index == (movie.production_countries.length - 1))
+        return country.name;
+      return `${country.name}, `;
+    });
+  }
+  render() {
+    const { movie } = this.props;
+
+    const genres = movie.genres ? this._renderGenres(movie) : null;
+    const production_companies = movie.production_companies ? this._renderProductionCompanies(movie) : null;
+    const production_countries = movie.production_countries ? this._renderProductionCountries(movie) : null;
 
     return (
       <View style={styles.container}>
@@ -38,7 +61,10 @@ class BackdropMovieDetail extends Component {
                 <RoundedButton color={colors.info} onPress={() => {}} text="Buy"/>
               </View>
               <Text style={styles.rating}>
-                Rating: {movie.vote_average}/10
+                Rating: {movie.vote_average}/10{' '}
+                <Text style={{fontSize: fonts.size.small}}>
+                  ({movie.vote_count})
+                </Text>
               </Text>
               <View>
                 <Rating
@@ -60,6 +86,10 @@ class BackdropMovieDetail extends Component {
               Release date: {' '}
               {movie.release_date}
             </Text>
+            <Text style={styles.movieReleaseDate}>
+              Countries: {' '}
+              {production_countries}{'.'}
+            </Text>
             <View>
               <Text style={styles.movieGenres}>
                 Genres:
@@ -71,6 +101,12 @@ class BackdropMovieDetail extends Component {
             </Text>
             <Text style={styles.movieOverview}>
               {movie.overview}
+            </Text>
+            <Text style={styles.movieOverviewTitle}>
+              Productions:
+            </Text>
+            <Text style={styles.movieOverview}>
+              {production_companies}{'.'}
             </Text>
           </View>
         </View>
