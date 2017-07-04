ac80485b5858fbfaa5f725143118be9a8bf6ed7f
Tri Pham <pvtri96@gmail.com>
2017-07-04 20:53:46 +0700

    Add PropTypes to AppNavigation

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { View, ActivityIndicator } from 'react-native';
 import { addNavigationHelpers } from 'react-navigation';
 import styles from './Styles/NavigationStyles';
@@ -16,6 +17,11 @@ const AppNavigation = ({nav, dispatch}) => (
     })}/>
   </View>
 );
+
+AppNavigation.propTypes = {
+  nav: PropTypes.object,
+  dispatch: PropTypes.func
+};
 
 const mapStateToProps = (state) => {
   return {
